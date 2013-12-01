8c9ca6c8f2d76a966696304a9e3f51e7461386a5
Oi Hirokazu <hiroo4001@ybb.ne.jp>
2013-12-01 20:23:16 +0900

    first rivision completed

--- file_char_code.pl
+++ file_char_code.pl
@@ -4,56 +4,167 @@
 # 2013/12/01
 # Oi Hirokazu
 #
+# 複数の文字コードが混ざったテキストファイルも判定できるよう，一行ずつ判定する。
+#
 # 参考：
 # 404 Blog Not Found:ruby|perl - 文字コードのちょっと高度な判定 : http://blog.livedoor.jp/dankogai/archives/50737353.html
 # 再帰的にすべてのファイルを処理する - File::Find::find() - サンプルコードによるPerl入門 : http://d.hatena.ne.jp/perlcodesample/20080530/1212291182
 #
 use strict;
 use warnings;
+use utf8;
+use Encode 'encode';
 use Encode::Guess;
 use File::Path;
 use Fcntl;
 use File::Find;
+use Getopt::Long;
+
+sub print_ascii_1 {
+    my $fh = shift;
+    for (my $i = 0; $i < 5; $i++) {
+	print $fh "abcde\n";
+    }
+}
+
+sub print_utf8_1 {
+    my $fh = shift;
+    for (my $i = 0; $i < 5; $i++) {
+	print $fh "こんにちは\n";
+    }
+}
+
+sub print_utf8_2 {
+    my $fh = shift;
+    print $fh "≫\n";
+    print $fh "損\n";
+    print $fh "≫損\n";
+    print $fh "≫Son\n";
+}
+
+sub print_eucjp_1 {
+    my $fh = shift;
+    my $str = encode('EUC-JP', 'こんにちは');
+    for (my $i = 0; $i < 5; $i++) {
+	print $fh "$str\n";
+    }
+}
+
+sub print_sjis_1 {
+    my $fh = shift;
+    my $str = encode('Shift_JIS', 'こんにちは');
+    for (my $i = 0; $i < 5; $i++) {
+	print $fh "$str\n";
+    }
+}
+
+sub print_u8_eu_1 {
+    my $fh = shift;
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "こんにちは\n";
+    }
+    my $str = encode('EUC-JP', 'こんにちは');
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "$str\n";
+    }
+}
+
+sub print_eu_sj_1 {
+    my $fh = shift;
+    my $str = encode('EUC-JP', 'こんにちは');
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "$str\n";
+    }
+    $str = encode('Shift_JIS', 'こんにちは');
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "$str\n";
+    }
+}
+
+sub print_sj_u8_1 {
+    my $fh = shift;
+    my $str = encode('Shift_JIS', 'こんにちは');
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "$str\n";
+    }
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "こんにちは\n";
+    }
+}
+
+sub print_u8_eu_sj_1 {
+    my $fh = shift;
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "こんにちは\n";
+    }
+    my $str = encode('EUC-JP', 'こんにちは');
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "$str\n";
+    }
+    $str = encode('Shift_JIS', 'こんにちは');
+    for (my $i = 0; $i < 3; $i++) {
+	print $fh "$str\n";
+    }
+}
 
 # テスト準備：ディレクトリとファイルの作成
-my $top_dir = "dir_20080530_$$";
-my @dirs = (
-  "$top_dir/dir1", "$top_dir/dir1/dir1_1", "$top_dir/dir1/dir1_2",
-  "$top_dir/dir2",
-);
-for my $dir (@dirs) {
-  eval { mkpath $dir };
-  if (@!) { die "@!" }
+sub create_testfiles {
+    my $top_dir = shift;
+    my @dirs = (
+	"$top_dir/ascii", "$top_dir/utf8", "$top_dir/eucjp",
+	"$top_dir/sjis", "$top_dir/mixed"
+	);
+
+    for my $dir (@dirs) {
+	eval { mkpath $dir };
+	if (@!) { die "@!" }
+    }
+
+    my %files = (
+	"$top_dir/ascii/ascii.txt" => \&print_ascii_1,
+	"$top_dir/utf8/utf8_1.txt" => \&print_utf8_1,
+	"$top_dir/utf8/utf8_2.txt" => \&print_utf8_2,
+	"$top_dir/eucjp/eucjp_1.txt" => \&print_eucjp_1,
+	"$top_dir/sjis/sjis_1.txt" => \&print_sjis_1,
+	"$top_dir/mixed/u8_eu_1.txt" => \&print_u8_eu_1,
+	"$top_dir/mixed/eu_sj_1.txt" => \&print_eu_sj_1,
+	"$top_dir/mixed/sj_u8_1.txt" => \&print_sj_u8_1,
+	"$top_dir/mixed/u8_eu_sj_1.txt" => \&print_u8_eu_sj_1,
+	);
+
+    for my $file (keys %files) {
+	sysopen( my $fh, $file, O_WRONLY | O_CREAT | O_EXCL )
+	    or die "$file を作成することができません。: $!";
+	&{$files{$file}}($fh);
+	close $fh;
+    }
+    print "created $top_dir\n\n";
 }
 
-my @files = (
-  "$top_dir/top.txt", "$top_dir/dir1/1.txt",
-  "$top_dir/dir1/dir1_1/1_1.txt", "$top_dir/dir1/dir1_2/1_2.txt",
-  "$top_dir/dir2/2.txt"
-);
-for my $file (@files) {
-  sysopen( my $fh, $file, O_WRONLY | O_CREAT | O_EXCL )
-    or die "$file を作成することができません。: $!";
-  close $fh;
-}
-print "準備: $top_dir を作成\n\n";
+#
+# main routine
+#
+my $create_testfiles_f;
+my $top_dir = ".";
+my $top_testdir = $top_dir . "/testfiles";
+GetOptions("testfiles" => \$create_testfiles_f);
+create_testfiles($top_testdir) if $create_testfiles_f;
 
-# 再帰下降
-print "再帰的に日本語文字コードを判定\n";
-$\ = "\n";
-find( \&determine_char_code, $top_dir );
+# 再帰下降して日本語文字コードを判定
+#$\ = "\n";
+find(\&determine_char_code, $top_dir);
 
 # 日本語文字コード判定処理                                   
 sub determine_char_code {
     return if $File::Find::name !~ /\.([hc]|java|txt|cpp)$/;
     my %hash = ();
-    open(my $fh, "<",  $File::Find::name) or die "Can't open $File::Find::name : $!";
     print "$File::Find::name\n";
-    while (my $line = readline $fh) {
+    open(my $fh, $_) or die "Can't open $File::Find::name : $!";
+    while (my $line = <$fh>) {
 	chomp $line;
 	my $enc = guess_encoding($line, qw/euc-jp shiftjis 7bit-jis/);
-	$enc_str = ref $enc ? $enc->name : $enc;
-	unless $hash{$enc_str} {
+	my $enc_str = ref $enc ? $enc->name : $enc;
+	unless ($hash{$enc_str}) {
 	    print "$enc_str\n";
 	    $hash{$enc_str} = 1;
 	}
